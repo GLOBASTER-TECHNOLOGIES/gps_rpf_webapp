0598ce3676b0d522005f69c334976d086fe1a38e
Noel Sebu <noelsebu555@gmail.com>
2025-12-16 11:00:42 +0530

    buttons for zoom in out and texture change added

---
+++ app/components/Buttons.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { FaMapMarkedAlt } from "react-icons/fa";
+import { MdSatelliteAlt } from "react-icons/md";
+import { LuZoomIn } from "react-icons/lu";
+import { LuZoomOut } from "react-icons/lu";
+
+
+
+
+type ButtonsProps = {
+    onZoomIn: () => void;
+    onZoomOut: () => void;
+    onToggleTexture: () => void;
+    isSatellite: boolean;
+};
+
+export default function Buttons({
+    onZoomIn,
+    onZoomOut,
+    onToggleTexture,
+    isSatellite,
+}: ButtonsProps) {
+    return (
+        <div className="fixed text-black bottom-14 right-6 z-[9999] flex flex-col gap-2 pointer-events-auto">
+            <button onClick={onToggleTexture} className="rounded bg-white px-3 py-2">
+                {/* {isSatellite ? "Normal Map" : "Satellite"} */}
+                {isSatellite ? (<div><FaMapMarkedAlt size={25} /></div>) : <div><MdSatelliteAlt size={27} /></div>}
+            </button>
+            <button onClick={onZoomIn} className="rounded bg-white px-4 py-2 font-bold">
+                < LuZoomIn size={20} />
+            </button>
+            <button onClick={onZoomOut} className="rounded bg-white px-4 py-2 font-bold">
+                < LuZoomOut size={20} />
+            </button>
+        </div>
+    );
+}
