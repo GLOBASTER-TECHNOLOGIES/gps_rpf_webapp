89e9ac3d980a0b638af17b480f0dd7f884de5b87
Noel Sebu <noelsebu555@gmail.com>
2025-12-16 12:02:43 +0530

    side bar logic added to navabr

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,61 @@
-import React from 'react'
+"use client";
 
-const Navbar = () => {
+import { FiMenu, FiSearch } from "react-icons/fi";
+import { useState } from "react";
+import Sidebar from "./Sidebar";
+
+export default function Navbar() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [showNavbar, setShowNavbar] = useState(true);
+
+  const openSidebar = () => {
+    setShowNavbar(false);   // hide navbar immediately
+    setSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setSidebarOpen(false);
+    setShowNavbar(true);
+    
+    // ⏳ wait for sidebar animation (300ms) before showing navbar
+    setTimeout(() => {
+      // setShowNavbar(true);
+    }, 200);
+  };
+
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      {/* Sidebar */}
+      <Sidebar open={sidebarOpen} onClose={closeSidebar} />
 
-export default Navbar
+      {/* Navbar */}
+
+      <div className={`fixed top-3 left-7 z-[5000] scale-110 ${sidebarOpen ? "opacity-0 duration-200" : "opacity-100 duration-300"} transition-all ease-in-out`}>
+        <div className="flex items-center gap-4 px-4 py-3">
+          {/* Burger Menu */}
+          <button
+            onClick={openSidebar}
+            className="bg-white text-gray-500 rounded-full p-2 hover:bg-gray-100 hover:scale-120 transition-all duration-100 ease-in-out border border-gray-300"
+            aria-label="Menu"
+          >
+            <FiMenu size={19} />
+          </button>
+
+          {/* Search Box */}
+          <div className="relative h-9 w-[26vw] max-w-[30vw] bg-white rounded-full border border-gray-300 flex items-center hover:scale-105 transition-all duration-100 ease-in-out">
+            <FiSearch
+              className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500"
+              size={19}
+            />
+            <input
+              type="text"
+              placeholder="Enter kilometer…"
+              className="w-full text-black rounded-full py-2 pl-10 pr-4 text-sm outline-none"
+            />
+          </div>
+        </div>
+      </div>
+
+    </>
+  );
+}
