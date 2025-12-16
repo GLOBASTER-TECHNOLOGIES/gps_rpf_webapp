b19fde3b948501d9d1ab647af38e7b7644c15d1a
Noel Sebu <noelsebu555@gmail.com>
2025-12-16 12:02:32 +0530

    navbar added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
 import Map from './components/Map'
+import Navbar from './components/Navbar'
 
 
 const page = () => {
   return (
     <div>
+      <Navbar />
       <Map />
     </div>
   )
